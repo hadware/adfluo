c908a36c3f5f05843dd3a2c2174396a406409606
hadware <hadwarez@gmail.com>
2021-03-24 03:59:07 +0100

    Cleaned up sample abstract class

--- adfluo/dataset.py
+++ adfluo/dataset.py
@@ -6,20 +6,6 @@ class Sample(ABC):
     _features: Dict[str, Any] = {}
     _dropped_features: Set[str] = set()
 
-    def __getitem__(self, item):
-        if not isinstance(item, str):
-            raise ValueError("Key has to be a string")
-
-        try:
-            return self.get_data(item)
-        except KeyError:
-            pass
-
-        try:
-            return self.get_feature(item)
-        except KeyError:
-            raise KeyError("Can't find data nor feature with such a name")
-
     @property
     @abstractmethod
     def id(self):
@@ -28,23 +14,11 @@ class Sample(ABC):
     def __hash__(self):
         return hash(self.id)
 
-    def get_data(self, data_name: str):
-        """This method should be overriden. Depending on the data that is being
-        'asked', returns the appropriate `
-        SampleData` instance or a raw value"""
+    @abstractmethod
+    def __getitem__(self, data_name):
+        """This method should be overridden. Depending on the data that is being
+        queried, returns the right value"""
         raise NotImplementedError()
-
-    def get_feature(self, feature_name: str):
-        return self._features[feature_name]
-
-    # TODO: figure out behavior with feature-storage objects
-    def store_feature(self, name, feature, drop_on_save=False):
-        """Stores the feature
-        In the future, this may cache 'heavy' features in a H5 file to
-        prevent overloading the memory"""
-        self._features[name] = feature
-        if drop_on_save:
-            self._dropped_features.add(name)
 
 
 class DictSample(Sample):
@@ -57,7 +31,7 @@ class DictSample(Sample):
     def id(self):
         return self.sample_id
 
-    def get_data(self, data_name: str):
+    def __getitem__(self, data_name):
         return self.sample_dict[data_name]
 
 
