c8f6d06eea03be7eae1304e6a86815ced49b1e2f
hadware <hadwarez@gmail.com>
2023-08-25 01:39:25 +0200

    Bumped version, added imports for hparam

--- adfluo/__init__.py
+++ adfluo/__init__.py
@@ -1,4 +1,4 @@
 from .dataset import DatasetLoader, Sample
-from .processors import SampleProcessor, F, Input, Feat, BatchProcessor, param
+from .processors import SampleProcessor, F, Input, Feat, BatchProcessor, param, hparam
 from .pipeline import ExtractionPipeline
 from .extractor import Extractor
